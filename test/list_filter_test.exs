21ea7af25361ee972d22a6ce0a3565945aefffa3
HiiMartins <higor.dsm10@gmail.com>
2021-12-14 01:15:10 -0300

    implementation of the ListFilterTest module

--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -2,7 +2,7 @@ defmodule ListFilterTest do
   use ExUnit.Case
   doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  test "Filter the integers in a list and return the number of odd numbers" do
+    assert ListFilter.odd_number_amounts(["1", "3", "6", "43", "banana", "6", "abc"]) == 3
   end
 end
