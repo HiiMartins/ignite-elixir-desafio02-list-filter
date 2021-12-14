8f339ccced659dc8390dbdf91d90573448018b7e
HiiMartins <higor.dsm10@gmail.com>
2021-12-14 00:05:06 -0300

    start project mix new

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ListFilter.MixProject do
     [
       app: :list_filter,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
