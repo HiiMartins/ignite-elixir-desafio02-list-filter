c6f6b0390ec46e230857a89a6a7e2fdc95f0d3f9
HiiMartins <higor.dsm10@gmail.com>
2021-12-14 01:08:00 -0300

    implementation of the ListFilter Module

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,18 +1,15 @@
 defmodule ListFilter do
-  @moduledoc """
-  Documentation for `ListFilter`.
-  """
+  defp filter_integers(list) do
+    Enum.flat_map(list, fn x ->
+      case Integer.parse(x) do
+        {int, _rest} -> [int]
+        :error -> []
+      end
+    end)
+  end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ListFilter.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def odd_number_amounts(list) do
+    list = filter_integers(list)
+    Enum.count(list, fn x -> rem(x, 2) != 0 end)
   end
 end
